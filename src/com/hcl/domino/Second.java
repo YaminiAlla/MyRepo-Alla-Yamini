ad8fd9b9a6eb2328a3ab7ed13c170f64ef4b69cf
YaminiAlla <63797197+YaminiAlla@users.noreply.github.com>
2020-05-04 22:22:19 +0530

    Add files via upload

--- src/com/hcl/domino/Second.java
+++ src/com/hcl/domino/Second.java
@@ -1,19 +1,24 @@
-package com.hcl.domino;
+package com.hcl.domino.case4;
 
-import com.hcl.domino.techassign1.FirstAccess;
+import com.hcl.domino.techassign1.case4.FirstAccess;
+
 /**
- * This class extends the main class i.e,Second extends FirstAccess class which is in
- * other package 
+ * This class extends the main class i.e,Second extends FirstAccess class which
+ * is in other package
  *
  */
 class Second extends FirstAccess {
 	public static void main(String[] args) {
-		//Displaying the program to user
-		System.out.println("Usage of the program\n The program is used to illustrate the access specifiers\n eg java Second ");
-
-		Second s = new Second(); //creating object to Second class
-		FirstAccess fa=new FirstAccess();  //creating object to FirstAccess class
-		System.out.println(fa.b + " " + fa.cube1(110)); //calling public method by using object
-		System.out.println(s.c + " " + s.cube2(120));  //calling protected method by using object
+		// Displaying the program to user
+		if (args.length == 0 || args[0].equals("-h") || args[0].equals("--help") || args[0].equals("-H")
+				|| args[0].equals("--HELP")) {
+			System.out.println(
+					"Usage of the program\n The program is used to illustrate the access specifiers\n eg java Second ");
+		} else {
+			Second s = new Second(); // creating object to Second class
+			FirstAccess fa = new FirstAccess(); // creating object to FirstAccess class
+			System.out.println(fa.b + " " + fa.cube1(110)); // calling public method by using object
+			System.out.println(s.c + " " + s.cube2(120)); // calling protected method by using object
+		}
 	}
 }
